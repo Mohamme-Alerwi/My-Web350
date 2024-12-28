06a0e7fa85379120bf7563dd055b7b6a8a255655
Mohamme-Alerwi <stdm_alerwi@outlook.com>
2024-12-28 13:02:25 +0300

    Add files via upload

---
+++ a/V.js
@@ -0,0 +1,34 @@
+
+// script.js
+document.getElementById('btn_1').addEventListener('click', function () {
+    var section = document.getElementById('sec_1');
+    if (section.style.display === 'none') {
+        section.style.display = 'block'; // إظهار القسم
+    } else {
+        section.style.display = 'none'; // إخفاء القسم
+    }
+});
+document.getElementById('btn_2').addEventListener('click', function () {
+    var section = document.getElementById('sec_2');
+    if (section.style.display === 'none') {
+        section.style.display = 'block'; // إظهار القسم
+    } else {
+        section.style.display = 'none'; // إخفاء القسم
+    }
+});
+document.getElementById('btn_3').addEventListener('click', function () {
+    var section = document.getElementById('sec_3');
+    if (section.style.display === 'none') {
+        section.style.display = 'block'; // إظهار القسم
+    } else {
+        section.style.display = 'none'; // إخفاء القسم
+    }
+});
+document.getElementById('btn_4').addEventListener('click', function () {
+    var section = document.getElementById('sec_4');
+    if (section.style.display === 'none') {
+        section.style.display = 'block'; // إظهار القسم
+    } else {
+        section.style.display = 'none'; // إخفاء القسم
+    }
+});
